ce02b725987861387cf17909da4ba9c1b2545985
Aaron Thomas <84493696+arxhr007@users.noreply.github.com>
2024-08-02 12:16:27 +0530

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+if [[ $(id -u) -ne 0 ]] ; then
+	printf "you need root assess to uninstall the tool\n\n\n"
+	printf "so please enter the password to login as root!\n\n"
+	sudo bash ${0}
+	exit
+fi
+rm /usr/bin/wifistrike &>/dev/null
+
+printf "\nuninstalled successfully!\n"
